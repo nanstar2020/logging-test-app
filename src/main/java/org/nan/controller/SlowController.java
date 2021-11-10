aa359bcaea085db8ad326b71fd66097dc6a103cd
nan <xiaonan@yahoo.com>
2021-11-10 17:38:33 +0000

    Modify Slow

--- src/main/java/org/nan/controller/SlowController.java
+++ src/main/java/org/nan/controller/SlowController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SlowController {
     @GetMapping("/slow")
     public String slowRequest() {
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-
-        }
+        long end = System.nanoTime() + 2 * 1000 * 1000 * 1000L;
+        do {
+            for (int i = 0; i < 1000; i++) {
+                int j = 100;
+                j = j * 1;
+            }
+        } while(System.nanoTime() < end);
 
         return "Done";
     }
