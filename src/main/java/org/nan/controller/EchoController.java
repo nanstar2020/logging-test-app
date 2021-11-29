d79dd22c38eb338a6231ace191541aff906f9b8e
Nan <xiaonan@yahoo.com>
2021-11-29 12:26:31 -0800

    Added JSON logging

--- src/main/java/org/nan/controller/EchoController.java
+++ src/main/java/org/nan/controller/EchoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.List;
 
 
@@ -28,6 +31,25 @@ public class EchoController {
 	public String echo(@PathVariable("message") String message) {
 		logger.debug(message);
 		return message;
+	}
+
+	@GetMapping(value="/logJson")
+	public String logJson() {
+		JSONObject object = new JSONObject();
+		object.put("id", 1L);
+		object.put("name", "Alice");
+		object.put("age", 20);
+
+		JSONArray courses = new JSONArray();
+		courses.put("Engineering");
+		courses.put("Finance");
+		courses.put("Chemistry");
+
+		object.put("courses", courses);
+
+		String jsonString = object.toString(2);
+		logger.info("Trying to log a json \n Here is the json: \n" + jsonString);
+		return "JSON logged";
 	}
 
 	@GetMapping(path="/createError")
